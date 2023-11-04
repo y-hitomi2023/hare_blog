5bf7427be2534b55e21096e3486728bbccdc9d90
y-hitomi2023 <as8stream@gmail.com>
2023-11-05 08:43:22 +0900

    N+1問題対策

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->paginate(4);
+        $posts = Post::with('user')->latest()->paginate(4);
         return view('posts.index', compact('posts'));
     }
 
